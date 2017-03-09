49a07cde4715ca57b4e0b8212cf753af541f19a7
Julien Martin <julienmartin.opto@gmail.com>
2017-03-09 13:43:29 +0100

    fix du bug JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-$(function() {
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 250){
-            $('#logo-mini').removeClass('hidden');
-        }else{
+function logo() {
+    var scroll = $(window).scrollTop();
+    if ($(window).width() > 1200) {
+        if (scroll <= 250) {
             $('#logo-mini').addClass('hidden');
+        } else {
+            $('#logo-mini').removeClass('hidden');
         }
-    });
+    };
+}
+
+logo();
+
+$(function() {
+    if ($(window).width() > 1200) {
+        $(window).scroll(function () {
+            logo();
+        });
+    };
 });
