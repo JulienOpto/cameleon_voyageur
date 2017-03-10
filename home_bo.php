0df711e81a63f4e51906ded431f07c5810951875
WCSIsabelle <ismuligo@gmail.com>
2017-03-10 10:06:05 +0100

    Modif vidéo home b-o

--- home_bo.php
+++ home_bo.php
@@ -5,7 +5,7 @@ include 'header_bo.php';
 ?>
 
 <div class="row vid">
-    <iframe width="853" height="480" src="https://www.youtube.com/embed/HQbx53g6O2k?rel=0" frameborder="0" allowfullscreen></iframe>
+    <iframe width="853" height="480" src="https://cut-wellcut-tv.azureedge.net/clips/5/c/7/5c7861b9-2dc7-4e2e-a212-c264b3f3ed5f.mp4" frameborder="0" allowfullscreen></iframe>
 </div>
 
 
